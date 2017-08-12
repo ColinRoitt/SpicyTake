469a3c71d31a31b19c424bce63c7003d654baf4d
Colin Roitt <me@colinroitt.uk>
2017-08-12 21:50:17 +0100

    secured my dang keys....

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-
+include('secret_keys.php');
 return [
 
     // key and secret of your application
-    'consumer_key'      => 'gkeDs7ZzNGo4kDGMzaUO2y6SV',
-    'consumer_secret'   => 'BakxBqiv9DSP4UeQ4fLNTDoViILhM71FMISVJuhTvxAI3fXk12',
+    'consumer_key'      => $consumer_key,
+    'consumer_secret'   => $consumer_secret,
 
     // callbacks for your application
     'url_login'         => 'http://dev.colinroitt.uk/spicytake/twitter_login.php',
