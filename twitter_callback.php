b589155c0a33f31062df49c2827bd558f80f5e59
cooltennis01 <cooltennis01@users.noreply.github.com>
2017-08-12 16:53:42 +0100

    style changes

--- twitter_callback.php
+++ twitter_callback.php
@@ -1,79 +1,6 @@
 <?php
 
-function getTweet(){
-    $tweet = 'Hot Take: ';
-    //set of perameters
-    //
-    $options = array(
-        $noun = array(
-            $politician = array(
-                "Donald J Trump",
-                "Hillary Clinton",
-                "Ted Cruz",
-                "David Cameron",
-                "Davey Cameron",
-                "Nicola Sturgeon",
-                "Boris Johnson",
-                "Micheal Gove",
-                "Nick Clegg",
-                "Paul Nuttel",
-                "Nigel Farage",
-                "Vladimir Putin",
-                "Francioise Holland",
-                "Damian Hinds",
-                "Angela Merkel",
-                "Malcolm Turnbull",
-                "Jeremy Corbyn",
-                "Bernie Sanders"
-            ),
-
-            $things = array(
-                "Jaffa cakes",
-                "Baseball bats",
-                "Video Games",
-                "Computers",
-                "Frogs",
-                "Music songs that are too loud",
-                "???",
-                "Pens"
-
-            )
-        ),
-
-        $is_a_thing = array(
-            "unfit for office",
-            "a liberal cuck ball",
-            "the zodiac killer",
-            "going to kill us all",
-            "secretly a lizard",
-            "bad",
-            "a fruit",
-            "???",
-            "coming",
-            "a sandwich",
-            "the Minecraft of sex",
-            "good actually",
-            "actually hozier",
-            "the real president"
-        )
-        
-    );
-
-    $polit_or_thing = rand(0,1);
-    $first_part_index = $options[0][$polit_or_thing];
-    $first_part_text = $first_part_index[rand(0,sizeof($first_part_index) - 1)];
-
-    $second_part_text = $options[1][rand(0,sizeof($options[1]) - 1)];
-
-    $connective = ' is ';
-    if($polit_or_thing == 1){
-        $connective = ' are ';
-    }
-
-    $tweet = print_r("Hot Take: ", true) . print_r($first_part_text, true) . print_r($connective, true) . print_r($second_part_text, true) . print_r(" #HotTakeBot http://dev.colinroitt.uk/spicytake", true);
-
-    return print_r($tweet, true);
-}
+include("generate_tweet.php");
 
 require 'twitteroauth/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
@@ -130,7 +57,49 @@ $status = $twitter->post(
     ]
 );
 
-echo ('Created new status successfully <br/>' . $tweet_to_post);
-echo("<br/><a href = 'http://dev.colinroitt.uk/spicytake'><h3>Link Home</h3></a>");
+// echo ('Created new status successfully <br/>' . $tweet_to_post);
+// echo("<br/><a href = 'http://dev.colinroitt.uk/spicytake'><h3>Link Home</h3></a>");
 
 ?>
+<html>
+    <head>
+        <link type="text/css" href="./style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class='center-post-tweet'>
+            <h1>Here is your <em>SPICY</em> hot take</h1>
+            <h3>It's been posted to your Twitter for you so you don't have to</h3>
+            <div id="tweet" tweetID=<?php echo($status->id) ?>  ></div>
+
+            <script sync src="https://platform.twitter.com/widgets.js"></script>
+
+            <script>
+
+            window.onload = (function(){
+
+                var tweet = document.getElementById("tweet");
+                var id = tweet.getAttribute("tweetID");
+                console.log(id);
+
+                twttr.widgets.createTweet(
+                id, tweet, 
+                {
+                    conversation : 'none',    // or all
+                    cards        : 'hidden',  // or visible 
+                    linkColor    : '#cc0000', // default is blue
+                    theme        : 'light'    // or dark
+                })
+                .then (function (el) {
+                el.contentDocument.querySelector(".footer").style.display = "none";
+                });
+
+            });
+
+            </script>
+
+            <br/><br/>
+            <a href="http://dev.colinroitt.uk/spicytake">Try Aagin</a>
+        </div>
+        
+    </body>
+</html>
